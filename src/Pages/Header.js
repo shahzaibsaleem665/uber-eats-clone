d70ad3c08d90d3c00bcc90626cda47c9cea48d83
shahzaib saleem <shahzaibsaleem665@gmail.com>
2024-01-04 21:14:47 +1100

    working on Navbar/Drawer

--- src/Pages/Header.js
+++ src/Pages/Header.js
@@ -1,17 +1,50 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Header.css'
 import { Button } from '@mui/material'
 import PersonIcon from '@mui/icons-material/Person';
 import MenuIcon from '@mui/icons-material/Menu';
 import FmdGoodIcon from '@mui/icons-material/FmdGood';
+import {
+  Drawer,
+  ListItem,
+} from '@mui/material';
 
 
 
-function Header() {
+
+function Header() { 
+     const [open, setOpen] = useState(false);
+  const getList = () => (
+      <div style={{ width: 300 }} onClick={() => setOpen(false)} className='navbar__button'>
+          <ListItem>
+          <Button className='listItem__register'>Sign up</Button>
+          </ListItem>
+          <ListItem>
+          <Button className='listItem__login'>Log in</Button>
+          </ListItem>
+          <div className="navbar__links">
+            <ListItem>
+              <p>Create a business account</p>
+            </ListItem>
+            <ListItem>
+              <p>Add your account</p>
+            </ListItem>
+            <ListItem>
+              <p>Sign up to deliver</p>
+            </ListItem>
+          </div>
+          <div className="navbar__bottom">
+          <ListItem>
+              <img />
+            </ListItem>
+          </div>
+      </div>
+    );
+  
   return (
     <div className='header'>
         <div className="header__left">
-            <MenuIcon />
+        <MenuIcon onClick={() => setOpen(true)} />
             <h1>Uber Eats</h1>
         </div>
         <div className="header__middle">
@@ -22,6 +55,13 @@ function Header() {
             <Button className='header__signIn'> <PersonIcon />Log in</Button>
             <Button className='header__signUp'>Sign up</Button>
         </div>
+
+
+
+{/* The below is Material Drawer for which opens rom left */}
+      <Drawer open={open} anchor={"left"} onClose={() => setOpen(false)}>
+        {getList()}
+      </Drawer>
     </div>
   )
 }
